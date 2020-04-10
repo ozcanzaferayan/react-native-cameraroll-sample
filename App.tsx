b2c6f5b0a5cb64f70c4d9ae3330dc46d71267a96
Zafer AYAN <ozcanzaferayan@gmail.com>
2020-04-10 21:46:30 +0300

    Add project files

--- App.tsx
+++ App.tsx
@@ -1,118 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  Platform,
+  PermissionsAndroid,
   SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  Button,
+  FlatList,
+  Image,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-declare var global: {HermesInternal: null | {}};
+import CameraRoll, {PhotoIdentifier} from '@react-native-community/cameraroll';
 
 const App = () => {
+  const [images, setImages] = useState<PhotoIdentifier[]>([]);
+
+  const requestStoragePermision = () => {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      {
+        title: 'Fotoğraf izni',
+        message:
+          'Fotoğraf görüntüleme için dosya okuma izni vermeniz gereklidir.',
+        buttonNeutral: 'Daha sonra sor',
+        buttonNegative: 'İptal',
+        buttonPositive: 'Tamam',
+      },
+    )
+      .then((granted) => {
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('İzin verildi');
+          getPhotos();
+        } else {
+          console.log('İzin verilmedi');
+        }
+      })
+      .catch((e) => console.log(e));
+  };
+  const handleButtonPress = () => {
+    if (Platform.OS === 'android') requestStoragePermision();
+    else getPhotos();
+  };
+  const getPhotos = () => {
+    CameraRoll.getPhotos({
+      first: 25,
+      assetType: 'Photos',
+    })
+      .then((r) => {
+        console.log(r);
+        setImages(r.edges);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView>
+      {images.length === 0 && (
+        <Button title="Galeriyi aç" onPress={handleButtonPress} />
+      )}
+      <FlatList
+        data={images}
+        keyExtractor={(data) => data.node.image.uri}
+        numColumns={3}
+        renderItem={(data) => (
+          <Image
+            style={{height: 200, flex: 1}}
+            source={{uri: data.item.node.image.uri}}
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
